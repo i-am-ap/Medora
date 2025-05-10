c1e63e2b3a7b8ab5dc7ccf16569b621d08fa5d4b
Aryan Palaspagar <21107040.aryan.palaspagar@gmail.com>
2025-05-10 13:43:40 +0530

    Update settings.py

--- DiabetesPrediction/settings.py
+++ DiabetesPrediction/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-!!2)z4dzsgzvzggxgx_c(6tai_z#&grq$u19!-glf@muw85l=b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [Medora-for-u.onrender.com]
 
 
 # Application definition
